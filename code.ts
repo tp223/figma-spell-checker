e5119e0b54a20a2374b39bdbf654521bf67784d4
Tom <42806979+tp223@users.noreply.github.com>
2022-09-03 18:35:48 +0100

    FInd all text

--- code.ts
+++ code.ts
@@ -2,37 +2,32 @@
 // You can access browser APIs such as the network by creating a UI which contains
 // a full browser environment (see documentation).
 
+function findAllTextStringsAndIds(): { nodeText: string[]; nodeId: string[] } {
+  const textNodes = figma.root.findAll(
+    node => node.type === "TEXT" && node.characters.length > 0
+  ) as TextNode[];
+
+  const nodeId = textNodes.map(node => node.id);
+  const nodeText = textNodes.map(textNode => textNode.characters);
+
+  return { nodeText, nodeId };
+}
+
 // Runs this code if the plugin is run in Figma
 if (figma.editorType === 'figma') {
   // This plugin will open a window to prompt the user to enter a number, and
   // it will then create that many rectangles on the screen.
 
   // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
+  figma.showUI(__html__, { themeColors: true });
 
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < msg.count; i++) {
-        const rect = figma.createRectangle();
-        rect.x = i * 150;
-        rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(rect);
-        nodes.push(rect);
-      }
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    }
+  var allNodes = findAllTextStringsAndIds();
 
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
-  };
+  console.log("Loading all text elements");
+
+  for (var currentNode in allNodes.nodeText) {
+    figma.ui.postMessage(allNodes.nodeText[currentNode]);
+  }
 
 // If the plugins isn't run in Figma, run this code
 } else {
